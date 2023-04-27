fefeb60ff5a432e1bda687d2992f8f44618a1b5e
JosuMarinho <del22031@byui.edu>
2023-04-26 23:33:06 -0600

    Finish Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a series of numbers. Enter 0 to stop.");
+
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.Write("Number: ");
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                numbers.Add(input);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers entered.");
+            return;
+        }
+
+        int sum = 0;
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        double average = (double) sum / numbers.Count;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Max: " + max);
     }
 }
