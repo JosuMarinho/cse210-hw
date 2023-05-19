387242db353d05d7c5f6e9863c134531b781e9a6
JosuMarinho <del22031@byui.edu>
2023-05-18 23:29:41 -0600

    Learning03done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction(3, 4);
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction();
+
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}");    // Output: 3/4
+        Console.WriteLine($"Fraction 1 Decimal Value: {fraction1.GetDecimalValue()}");    // Output: 0.75
+
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}");    // Output: 5/1
+        Console.WriteLine($"Fraction 2 Decimal Value: {fraction2.GetDecimalValue()}");    // Output: 5
+
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}");    // Output: 1/1
+        Console.WriteLine($"Fraction 3 Decimal Value: {fraction3.GetDecimalValue()}");    // Output: 1
     }
 }
