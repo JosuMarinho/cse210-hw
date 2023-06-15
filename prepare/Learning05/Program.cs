f80ddf228eb99d2aa92262b8bdab44a36e50fa95
JosuMarinho <del22031@byui.edu>
2023-06-14 23:27:12 -0600

    learning 5 done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        // Add shapes to the list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterate and display the color and area of each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+
+        // Wait for the user to press Enter to close the application.
+        Console.ReadLine();
     }
 }
